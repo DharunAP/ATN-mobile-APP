ce635668a3c36378108c692f391e8aaedd4a4fa1
Guhan-K <guhan.k2022cse@sece.ac.in>
2024-04-14 16:11:34 +0530

    Header completed

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Terminal } from './components/Terminal'
+import { StyleSheet, Platform, StatusBar, SafeAreaView } from "react-native";
+import { Terminal } from "./components/Terminal";
+import RequestDispaly from "./components/RequestDisplay";
+import Header from "./components/Header";
 
 export default function App() {
-  return (
-    <View>
-      <Terminal/>
-    </View>
-  );
+    return (
+        <SafeAreaView style={styles.AndroidSafeArea}>
+            {/* <Terminal /> */}
+            <Header />
+            <RequestDispaly />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  
+    AndroidSafeArea: {
+        flex: 1,
+        backgroundColor: "#FFE6CF",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    },
 });
---
+++ components/RequestDisplay.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const RequestDispaly = () => {
+    return (
+        <>
+            <Text> Hello </Text>
+        </>
+    );
+};
+
+export default RequestDispaly;

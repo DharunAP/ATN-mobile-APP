fdcfeb3f2347f486b984e20627944f31526092ef
dharun <dharunpalanisamy5256@gmail.com>
2024-04-14 14:27:14 +0530

    First push

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Terminal } from './components/Terminal'
+
+export default function App() {
+  return (
+    <View>
+      <Terminal/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
---
+++ components/Terminal.js
@@ -0,0 +1,10 @@
+import { Text } from "react-native"
+
+
+export const Terminal = ()=>{
+    return (
+        <>
+            <Text>Hello</Text>
+        </>
+    )
+}
